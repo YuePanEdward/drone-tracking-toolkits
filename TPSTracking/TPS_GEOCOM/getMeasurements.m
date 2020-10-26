38fe64b71218332afeb07a678dc564a930a74f5e
Yue Pan <yuepan@student.ethz.ch>
2020-10-26 22:05:51 +0100

    Update getMeasurements.m

--- TPSTracking/TPS_GEOCOM/getMeasurements.m
+++ TPSTracking/TPS_GEOCOM/getMeasurements.m
@@ -1,5 +1,25 @@
-function [D, Hz, V] = getMeasurments(TPSport, distmode)
+function [D, Hz, V, ts] = getMeasurments(TPSport, distmode)
+% getMeasurments : get the measurements in spherical coordinate system with the timestamp
+% ts (unit: second, resolution: 0.01 s)
 
+    % Get the measurement timestamp
+        fprintf('CSV_GetDateTimeCentiSec - returning angles and distance measurement\n');
+        req='%R1Q,5117:';
+        fprintf (TPSport, req); 
+        
+        % Reply - read response data into the sting-variable out        
+        out = fscanf (TPSport);
+        data = regexp(out,':(?<RC>[^,]*),(?<Year>[^,]*),(?<Month>[^,]*),(?<Day>[^,]*),(?<Hour>[^,]*),(?<Minute>[^,]*),(?<Second>[^,]*),(?<CentiSecond>[^,]*)','names');
+        
+        ts_status =str2num(data.RC);
+        
+        if(ts_status==0)
+            ts=[data.Year data.Month data.Day data.Hour data.Minute data.Second '.' data.CentiSecond];
+            ts = str2double(ts);
+        end
+    
+    % Take angle and distance measurments
+    
     if ~isempty(distmode)   
         fprintf('TMC_GetAngle - returning angles and distance measurement\n');
         req=['%R1Q,17017:', num2str(distmode)];
@@ -20,6 +40,7 @@ function [D, Hz, V] = getMeasurments(TPSport, distmode)
             Hz = Hz * 180/pi; % in deg
             V= 90 - (Z * 180/pi);    % in deg (vertical angle)
 
+     
 % Print the values
             fprintf('D = %.4f [m]; Hz = %.4f [deg]; V = %.4f [deg]\n',D, Hz, V); % in deg
             if(meas_status == 1284)
